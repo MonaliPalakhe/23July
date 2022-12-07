cefa6f46f38546833ade611fadc4cef170e681b4
MonaliPalakhe <119988033+MonaliPalakhe@users.noreply.github.com>
2022-12-07 19:17:57 +0530

    Update Simple.java

--- 23july/src/Array/Simple.java
+++ 23july/src/Array/Simple.java
@@ -4,11 +4,11 @@ public class Simple {
 
 	public static void main(String[] args) {
 		int a[]=new int[5];
-		a[0]=1;
-		a[1]=4;
-		a[2]=3;
-		a[3]=6;
-		a[4]=7;
+		a[0]=10;
+		a[1]=45;
+		a[2]=33;
+		a[3]=67;
+		a[4]=79;
 		System.out.println(a[3]);
 		for(int i=0;i<5;i++) {
 			System.out.println(a[i]);
